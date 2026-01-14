1220c66113164dcaaf9ec49ad7d4432ae388c586
seangreen12333 <seantgreen64@gmail.com>
2026-01-14 17:46:41 +1030

    Update start-list-2026.js

--- pages/start-list-2026.js
+++ pages/start-list-2026.js
@@ -105,6 +105,94 @@ export default function StartList2026() {
   and claiming the Uni Loop crown.
 </p>
 
+{/* WOMEN'S ELITE RACE */}
+<h2 className="text-3xl font-bold mt-12">
+  Run the Loop – Women’s Elite field
+</h2>
+
+<p>
+  This year's edition of the Run The Loop will feature a strong elite ladies' field,
+  with an abundance of national-class junior middle-distance stars.
+</p>
+
+<p>
+  Record: Caitlin Adams – 6:47 (2025)
+</p>
+
+<h3 className="text-2xl font-semibold mt-6">
+  Amelia Sheridan
+</h3>
+
+<p>
+  In 2026, 14-year-old Millie doesn’t need a big introduction. A multiple national gold
+  medalist over the 1500/3000m and cross-country events, last year she ran a blistering
+  time (6:52) and was just 5 seconds behind senior national representative Caitlin Adams.
+  Millie continues to improve this year, producing a gun-to-tape national 1500m all
+  schools victory, with a huge PB of 4.22. Not only will she be looking to go one better
+  this year, but it would be no surprise if she covers the gravel loop in under the
+  course record on Friday night!
+</p>
+
+<h3 className="text-2xl font-semibold mt-6">
+  Tilly Williams
+</h3>
+
+<p>
+  A part of the reason the record will be under threat this year is because of another
+  junior talent, in Tilly Williams. There is zero doubt that Tilly will challenge for
+  the win, with her tenacious attacking running style! Another 14-year-old, Tilly, was
+  2nd to Millie in the national all schools 1500 (4.28). With great speed, endurance, and
+  running form, Tilly is always one to watch in any event from 800 to 5000m. There’s no
+  doubt that the quality Adelaide Harrier will enjoy the 2.2km distance!
+</p>
+
+<h3 className="text-2xl font-semibold mt-6">
+  Hannah Tourneur
+</h3>
+
+<p>
+  Last year’s 3rd place getter (7:07) and talented middle distance runner, Hannah, will
+  be out to improve on that result. Dual silver medallist in the 1500/3000m at the recent
+  U18 all schools in December, she has stepped up her training in the past 2 months and
+  will be a shot at the prestigious 7-minute barrier. However, with racing in her blood,
+  if she is given a small sniff after the loop climb (at the mile mark), Hannah has the
+  most devastating kick in the state, and you can not count out this girl until the
+  finish line.
+</p>
+
+<h3 className="text-2xl font-semibold mt-6">
+  Leni Knowles
+</h3>
+
+<p>
+  Primarily a state league basketballer, Leni has turned heads this season for a few
+  reasons! One, her fluent running form is a standout, and two, her recent silver medal
+  in the U15 girls national All Schools 800m (2.11). Yes, another SA girl in that talented
+  U15 age group. However, Leni brings absolute speed to this field (58s quarter miler) and
+  ever-improving endurance. It would not be a surprise if she’s 20-30 seconds quicker
+  this year than last year (7th in 7:33 in 2025).
+</p>
+
+<h3 className="text-2xl font-semibold mt-6">
+  Brooke Hines
+</h3>
+
+<p>
+  Dietician by trade, but distance runner by blood! Brooke spent time in the NCAA system
+  and is now back, mixing working life with her running! This season has seen some form
+  return to her. With lifetime PBs of 4:30 for 1500m and 9:40 for 3000m, Brooke will feel
+  at home here on the Uni loop! 29 is far from old, but the Saint’s President brings
+  probably the most racing experience to this field, and she’ll be aiming to push some
+  young ladies off the podium!
+</p>
+
+<p>
+  These talented girls will be joined by fellow star SA distance athletes: Gracie
+  Sheridan, Eva Flint, India Goodall, Elise Dandie, Lauren Ward, Eden Grant, Maeya
+  Robinson, Sofia Heyward, Hayley Redman, Pia Marschall &amp; Violette Smith.
+</p>
+
+    
       </section>
 
     {/* REGULATIONS / PRIZES */}
