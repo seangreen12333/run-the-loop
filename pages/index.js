8face7daf8a35ed0430eddc0a554319efe8ae636
seangreen12333 <seantgreen64@gmail.com>
2026-09-23 19:12:28 +0930

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
       {/* Main Hero Section */}
       <div
         className="relative min-h-screen bg-cover bg-center"
-        style={{ backgroundImage: "url('MN_27_stand_elite.jpg')" }}
+        style={{ backgroundImage: "url('Lightroom-5418.jpeg')" }}
       >
         {/* Dark overlay */}
         <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
