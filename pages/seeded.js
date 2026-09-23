808a6c5ecffb54592429c99f2d4e1cee0396cc76
seangreen12333 <seantgreen64@gmail.com>
2026-09-23 22:48:30 +0930

    Update seeded.js

--- pages/seeded.js
+++ pages/seeded.js
@@ -9,8 +9,9 @@ export default function SeededRacesPage() {
 
       {/* HERO IMAGE */}
       <section
-        className="relative h-[50vh] bg-cover bg-center flex items-center justify-center text-white"
-        style={{ backgroundImage: "url('/AUA_RH_DSC08488.jpeg')" }}
+        className="relative h-[50vh] bg-cover flex items-center justify-center text-white"
+        style={{ backgroundImage: "url('/AUA_RH_DSC08488.jpeg')",
+               backgroundPosition: "center 30%"}}
       >
         <h1 className="text-5xl font-extrabold bg-black/60 px-4 py-2 rounded">
           Seeded Races
